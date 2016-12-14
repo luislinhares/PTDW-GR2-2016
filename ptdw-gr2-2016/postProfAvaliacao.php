752769e6b3878a544a9e166f33793a7d94460cc2
Luis Linhares <luis.linharesr@ua.pt>
2016-12-14 02:57:08 +0000

    first commit

---
+++ ptdw-gr2-2016/postProfAvaliacao.php
@@ -0,0 +1,132 @@
+<html lang="pt">
+
+<head>
+    <meta charset=utf-8>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>3R - Profissionais de Saúde</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/business-frontpage.css" rel="stylesheet">
+	<link href="css/profissionais.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+	<!-- Navigation -->
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+			<a class="navbar-brand" href="index.html"><img src="img/logo_3r_verde.jpg" alt="3R" height="50" width="50" border="0" style="position: absolute; top: 0px;"></a><a class="navbar-brand hidden-xs hidden-sm titulo" href="index.html">Plataforma de Reabilitação <br>Respiratória em Rede</a>
+		  </div>
+			
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li><a href="postProf.php"> <span class="glyphicon glyphicon-home"></span> </a></li>
+					<li class="active"><a href="postProfAvaliacao.php">Avaliação</a></li>
+					<li><a href="#">Exercício Físico</a></li>
+					<li><a href="#">Apoio Psicossocial</a></li>
+					<li><a href="index.html#contactos">Contactos</a></li>
+                </ul> 
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+	    
+	<header class="header">
+        <div class="text-header">
+			<div class="container">
+			<span>Profissionais de Saúde</span>
+		</div>
+		</div>
+    </header>
+		
+    <!-- Page Content -->
+	<div class="container">				
+		<div class="post" style="position: relative; top: 0px;">
+			<br>
+			<div class="crumbs"><a href="#">Home </a>> <a href="#">Avaliação</a></div>
+			<hr>
+			<div class="row">
+				<div class="col-md-3">
+					<div class="list-group">
+						<a href="#" class="list-group-item active">Com que frequência?</a>
+						<a href="#" class="list-group-item">Como se organiza?</a>
+						<a href="#" class="list-group-item">Que domínios?</a>
+					</div>
+				</div>
+						
+				<div class="col-md-8">
+					<!-- Post Content -->
+					<p class="lead">Com que frequência?</p>
+					<p>A reabilitação respiratória deve contemplar uma avaliação compreensiva inicial e após o programa de reabilitação respiratória (8-12 semanas, dependendo da duração do mesmo), do paciente e família, de forma a ser possível estabelecer o programa individualizado da reabilitação respiratória bem como avaliar os resultados obtidos com o mesmo.</p>
+					<p>Deve também contemplar-se a avaliação do próprio programa junto dos paciente e família.</p>
+					<p>Durante cada sessão de exercício físico deve haver uma avaliação inicial e final e monitorização dos pacientes de forma a que as sessões decorram dentro dos limites terapêuticos de segurança.</p>
+				</div>
+				<div class="col-md-1">
+					<div class="glyphicon glyphicon-minus" id="btnMinus"></div>
+					<div class="glyphicon glyphicon-plus" id="btnPlus"></div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+	
+	<!-- Footer -->
+	<footer class="fprof">
+		<div class="row">
+			<div class="col-lg-3"></div>
+			<div class="col-lg-3">
+				<h3>Plataforma 3R</h3>
+				<li><a href="postPac.php">Pacientes / Familiares</a></li>
+				<li><a href="postProf.php">Profissionais de Saúde</a></li>
+				<li><a href="index.html#contactos">Contactos</a></li>
+			</div>
+			<div class="col-lg-3">
+				<h3>Links Úteis</h3>
+				<li><a href="http://www.saude24.pt">Saúde 24</a></li>
+				<li><a href="http://www.sppneumologia.pt">Sociedade Portuguesa de Pneumologia</a></li>
+				<li><a href="http://www.fundacaoportuguesadopulmao.org">Fundação Portuguesa do Pulmão</a></li>
+				<li><a href="http://www.respira.pt">Respira</a></li>
+			</div>
+			<div class="col-lg-3"></div>
+		</div>
+		<div class="row" style="padding-top: 20px;">
+			<div class="col-lg-12" align="center">
+				<p>Copyright &copy; Grupo 2 PTDW 2016</p>
+			</div>
+		</div>
+	</footer>
+			
+	<!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+	<!-- Accessibility -->
+	<script src="js/accessibility.js"></script>
+
+</body>
+
+</html>
